b43f3beb46a01d01fe0c794bc47402526a4affcc
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-06-21 21:05:05 +0530

    Added Login System also

--- LoginApp/app/src/main/java/com/devlovepreet/loginapp/BackgroundTask.java
+++ LoginApp/app/src/main/java/com/devlovepreet/loginapp/BackgroundTask.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
+import android.widget.EditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,8 +20,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -28,6 +32,7 @@ import java.net.URLEncoder;
  */
 public class BackgroundTask extends AsyncTask<String,Void,String> {
     String register_url = "http://10.32.10.198/loginapp/register.php";
+    String login_url = "http://10.32.10.198/loginapp/login.php";
     Context ctx;
     Activity activity;
     ProgressDialog progressDialog;
@@ -97,6 +102,57 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
                 e.printStackTrace();
             }
         }
+        else if(method.equals("login"))
+        {
+            try {
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                //send data to server
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter =  new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+
+                String email,password;
+                email = params[1];
+                password = params[2];
+                String data = URLEncoder.encode("email","UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                //get response from server
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = "";
+                while((line=bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(line+"\n");
+                }
+                httpURLConnection.disconnect();
+                Thread.sleep(5000);
+                return stringBuilder.toString().trim();
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
         return null;
     }
 
@@ -122,6 +178,16 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
             {
                 showDialog("Registration Failed",message,code);
             }
+            else if(code.equals("login_true"))
+            {
+                Intent intent = new Intent(activity,HomeActivity.class);
+                intent.putExtra("message",message);
+                activity.startActivity(intent);
+            }
+            else  if(code.equals("login_false"))
+            {
+                showDialog("Login Failed",message,code);
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -142,8 +208,25 @@ public class BackgroundTask extends AsyncTask<String,Void,String> {
                     activity.finish();
                 }
             });
-            AlertDialog alertDialog = builder.create();
-            alertDialog.show();
+
+        } else if(code.equals("login_false"))
+        {
+            builder.setMessage(message);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    EditText email,password;
+                    email = (EditText) activity.findViewById(R.id.email);
+                    password = (EditText) activity.findViewById(R.id.password);
+                    email.setText("");
+                    password.setText("");
+                    dialog.dismiss();
+
+                }
+            });
+
         }
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
